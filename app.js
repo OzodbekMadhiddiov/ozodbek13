cff3d422af67a5aa32dd6b685727926d2a0fe3ed
OzodbekMadhiddinov <madozodbekmad84@gmail.com>
2023-05-10 10:11:53 +0500

    html va javascript git hubga joyladik

---
+++ app.js
@@ -0,0 +1,27 @@
+// alert(" gsadga")
+
+// confirm("davom etirasizmi")
+
+// prompt("kodni kiriting")
+
+// let name = prompt("isminigizni kiriting")
+// // let name = "ozod";
+
+
+// console.log(name);
+
+// let name = prompt('ismingizni kiriting')
+// let name1 = prompt(' familiyaqgizni kiriting ')
+// // let age = prompt('yoshingizni kiriting  ')
+// // confirm("ismingiz: " + name + " " + "fomiliyangiz: " + name1 + " " + "yoshingiz: " + age  )
+
+// let a = 10
+// let b = 15
+// console.log(a + b )
+// let result = (100 + 1) * 3;
+// console.log(result);
+// console.log(2**4)
+
+// let a = 2
+// console.log(a++)
+
